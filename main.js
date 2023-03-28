bef08664a7a3984353b3486c0eff12eb474012ef
NertonSaintfort <SaintfortNerton@gmail.com>
2023-03-28 18:13:23 -0400

    final commit

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 //todo:
 /* Getting the elements from the HTML file. */
-let addToDoButton = document.getElementById('addToDo')
-let toDoContainer = document.getElementById('toDoContainer')
-let inputField = document.getElementById('inputField')
+let addToDoButton = document.getElementById("addToDo");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField");
 
 /* Creating a new paragraph element and adding a class to it. */
-addToDoButton.addEventListener('click', function () {
-  var paragraph = document.createElement('p')
-  paragraph.classList.add('paragraph-styling')
+addToDoButton.addEventListener("click", function () {
+  var paragraph = document.createElement("p");
+  paragraph.classList.add("paragraph-styling");
   /* Taking the value of the input field and adding it to the paragraph. */
-  paragraph.innerText = inputField.value
-  toDoContainer.appendChild(paragraph)
-  inputField.value = ''
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
   /* Adding an event listener to the paragraph element. When the paragraph is clicked, the text
 decoration is changed to line-through. */
-  paragraph.addEventListener('click', function () {
-    paragraph.style.textDecoration = 'line-through'
-  })
+  paragraph.addEventListener("click", function () {
+    paragraph.style.textDecoration = "line-through";
+  });
   /* Adding an event listener to the paragraph element. When the paragraph is double clicked, the
 paragraph is removed from the toDoContainer. */
-  paragraph.addEventListener('dblclick', function () {
-    toDoContainer.removeChild(paragraph)
-  })
-})
+  paragraph.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(paragraph);
+  });
+});
